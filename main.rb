3506497dc41347b857e7182be01be5d3bdb98be2
Alex Sears <alexwsears@gmail.com>
2016-05-19 22:00:02 -0400

    Added handling for when token not passed

--- main.rb
+++ main.rb
@@ -11,13 +11,15 @@ class JwtAuth
   def call env
     begin
       options = { algorithm: 'HS256', iss: ENV['JWT_ISSUER'] }
-      bearer = env["HTTP_AUTHORIZATION"][8..-1]
+      bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(8..-1)
       payload, header = JWT.decode bearer, ENV['JWT_SECRET'], true, options
 
       env[:scopes] = payload['scopes']
       env[:user] = payload['user']
 
       @app.call env
+    rescue JWT::DecodeError
+      [401, { 'Content-Type' => 'text/plain' }, ['A token must be passed.']]
     rescue JWT::ExpiredSignature
       [403, { 'Content-Type' => 'text/plain' }, ['The token has expired.']]
     rescue JWT::InvalidIssuerError
